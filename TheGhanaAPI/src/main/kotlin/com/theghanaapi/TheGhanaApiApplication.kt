8087ceb6df395a6de5b88cbb6685420fc0e5e820
Isaac Owusu <kwabenacross@gmail.com>
2023-04-01 19:50:39 +0000

    Initial Commit

---
+++ TheGhanaAPI/src/main/kotlin/com/theghanaapi/TheGhanaApiApplication.kt
@@ -0,0 +1,11 @@
+package com.theghanaapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TheGhanaApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<TheGhanaApiApplication>(*args)
+}
---
+++ TheGhanaAPI/src/test/kotlin/com/theghanaapi/TheGhanaApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.theghanaapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TheGhanaApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
